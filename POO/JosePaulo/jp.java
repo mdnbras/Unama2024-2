7fccfb681abe1cdfd361607c05d4ee5f893e2c30
JP <josepaulo1607@gmail.com>
2024-08-30 19:24:58 -0300

    tarefa

---
+++ POO/JosePaulo/jp.java
@@ -0,0 +1,19 @@
+
+public class jp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
+public class Tarefa {
+    private String titulo;
+    private String Descricao;
+    private String prazo;
+    private int propriedade;
+    //metodo get
+    public String getTitulo() {
+        return this, titulo;
+    }
+
+    
+}
